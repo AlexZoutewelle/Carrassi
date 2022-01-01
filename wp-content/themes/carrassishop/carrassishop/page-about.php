229d4052827034b684ab8343b2c5db14e1d9e392
Alex <alex@arkdesign.nl>
2022-01-01 22:59:13 +0100

    Working on individual portfolio items

--- wp-content/themes/carrassishop/carrassishop/page-about.php
+++ wp-content/themes/carrassishop/carrassishop/page-about.php
@@ -5,7 +5,7 @@ get_header();
     <button class="btn-toTop"><i class="fas fa-arrow-up"></i></button>
 </aside>
 
-<main id="primary" class="site-main">
+<main id="primary" class="site-main about_page">
     <section class="about_page_intro">
         <div class="container" style="position:relative;">
             <div class="row pt-5">
@@ -19,17 +19,18 @@ get_header();
                         <label>
 
                             <h3>
-                                And I develop Wordpress plugins
+                                I develop Wordpress plugins
                             </h3>
-                            <p>
-                                I'm not a frontend designer. I'm not the one that'll make your website look cute. <br>
-                                I'm the one that takes care of <span>custom functionality</span>. <br><br>
-                                • When your webshop needs a more advanced search module.<br>
-                                • When you need a system for sending PDF invoices in bulk.<br>
-                                • When your administration in Woocommerce Bookings simply takes too much time.
-
-                            </p>
+                            <hr>
+                            <h5>
+                                I'm not a designer. I'm not the one that'll make your website look cute. <br>
+                                I build custom functionality. The components you need to do your job.<br><br>
+                            </h5>
                         </label>
+
+                        <button type="button" class="btn btn-yellow">
+                            Check out my past work
+                        </button>
 
                     </div>
 
@@ -37,37 +38,65 @@ get_header();
         </div>
     </section>
 
-    <section class="about">
+
+    <section class="about_page_item" id="availability_manager">
         <div class="container" style="position:relative;">
-            <div class="row">
-
-
-            </div>
-        </div>
-    </section>
-
-
-    <section class="about">
-        <div class="container" style="position:relative;">
-            <div class="row">
+            <div class="row h-100 d-flex align-items-center">
                 <div class="col-5">
-                    <img src="http://carrassi/wp-content/uploads/2021/12/wOezUV1n_400x400-removebg-preview-1.png"/>
+                    <h1>Availability Manager</h1>
+                    <hr class="hr-red">
 
+                    <!--                    <h3>A Woocommerce Bookings extension</h3>-->
+                    <p>The Woocommerce Bookings plugin had everything the client needed, except that it proved unwieldy in their use case. They needed a way to change data in bulk, saving heaps of time and preventing crucial user errors.</p>
+                    <a href='https://ide.geeksforgeeks.org/'>
+                        <button class="btn_about_page_item red">
+                            Case-study
+                        </button>
+                    </a>
                 </div>
                 <div class="col-7">
-                    <h1 class="intro_h1">Hey Im alex
-                    </h1>
-                    <h3>
-                        I develop Wordpress plugins
-                    </h3>
-                    <p>
-                        And i do all kinds of shit yess
-                    </p>
+                    <div class="image_base red" style="position:relative;">
+                        <div>
+                            <img class="about_page_screenshot" src="http://carrassi/wp-content/uploads/2022/01/chrome_8hROCQN5D8.png"/>
+                            <div class="about_page_overlay"
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
     </section>
+
+
+
+    <section class="about_page_item" id="invoice_system">
+        <div class="container" style="position:relative;">
+            <div class="row h-100 d-flex align-items-center">
+                <div class="col-5">
+                    <h1>Custom Invoice System</h1>
+                    <hr class="hr-blue">
+
+                    <!--                    <h3>A Woocommerce Bookings extension</h3>-->
+                    <p>The client wanted a complete system for managing invoices based on their Woocommerce Bookings plugin. The idea: generate .PDF invoices from all currently unpaid bookings and send them periodically via e-mail, including reminders and warnings.</p>
+                    <a href='https://ide.geeksforgeeks.org/'>
+                        <button class="btn_about_page_item blue">
+                            Case-study
+                        </button>
+                    </a>
+                </div>
+                <div class="col-7">
+                    <div class="image_base blue" style="position:relative;">
+                        <div>
+                            <img class="about_page_screenshot" src="http://carrassi/wp-content/uploads/2022/01/chrome_8hROCQN5D8.png"/>
+                            <div class="about_page_overlay"
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
 </main>
 
 <?php
